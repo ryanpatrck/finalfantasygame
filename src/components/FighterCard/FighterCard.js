715d84945214bf1b7f607edc82db46530d7b89db
Ryan <Admin@MacBook-Pro.local>
2018-06-29 18:41:54 -0400

    css changes

--- src/components/FighterCard/FighterCard.js
+++ src/components/FighterCard/FighterCard.js
@@ -4,7 +4,7 @@ import "./FighterCard.css";
 const FighterCard = props => (
     <div className="card">
         <div className="img-container">
-        <a onClick={()=> props.selectFighter(props.name)} 
+        <a onClick={()=> props.selectFighter(props.fighter)} 
             className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
             >
             <img alt={props.name} src={props.image} />
